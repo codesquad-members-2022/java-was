f3d319d2c737bd9704d796f4f93811e68d9c4eda
mybloom <baejungeun11@gmail.com>
2022-03-21 12:40:40 +0900

    feat: GET /index.html 클라이언트 요청에 따른 index.html 응답
    - resources/webapp/index.html 파일 생성
    - RequestHandler.run() 메서드에서 HTTP request 메세지 한 줄을 읽어 tokens로 파싱한다.
    - 파싱 문자열 배열에서 URI에 해당되는 파일을 응답 메세지로 전송한다.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,8 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,9 +21,13 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String line = br.readLine();
+
+            String[] tokens = line.split(" ");
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = Files.readAllBytes(new File("./src/main/resources/webapp" + tokens[1]).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {

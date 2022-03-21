d5f40dcc9297ff84131573af76ae298fbafa94b0
hwicode <khc3796@naver.com>
2022-03-21 20:50:27 +0900

    정적인 html 파일 응답을 위해 RequestHandler를 수정함
    - HashMap을 통해 request header를 KEY 와 VALUE로 나눔

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,10 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,10 +12,19 @@ import org.slf4j.LoggerFactory;
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
+    private final int METHOD = 0;
+    private final int URL = 1;
+    private final int HTTP_VERSION = 2;
+    private final int KEY = 0;
+    private final int VALUE = 1;
+
     private Socket connection;
+    private String requestLine;
+    private Map<String, String> requestHeaderField;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
+        this.requestHeaderField = new HashMap<>();
     }
 
     public void run() {
@@ -23,13 +32,29 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            requestLine = br.readLine();
+            String[] tokens = requestLine.split(" ");
+            String url = tokens[URL];
+            setRequestHeader(br);
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
+        }
+    }
+
+    private void setRequestHeader(BufferedReader br) throws IOException {
+        String line;
+        while (!"".equals(line = br.readLine())) {
+            if (line == null) {
+                return;
+            }
+            String[] header = line.split(": ");
+            requestHeaderField.put(header[KEY], header[VALUE]);
         }
     }
 

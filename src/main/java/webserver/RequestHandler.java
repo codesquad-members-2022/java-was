b652321a15d54e094e4a98fa185f4d857a2e92f6
rkolx <olx.rko.o@gmail.com>
2022-03-21 17:43:13 +0900

    feat : 클라이언트 요청 페이지 응답 기능 추가
    - localhost:8080/{filename.html}을 요청하면 filename.html을 응답한다.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,8 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,13 +21,28 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            String url = getUrl(in);
+            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
+        }
+    }
+
+    private String getUrl(InputStream in) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        String line = br.readLine();
+        String[] tokens = line.split(" ");
+        showHeaders(br);
+        return tokens[1];
+    }
+
+    private void showHeaders(BufferedReader br) throws IOException {
+        String line;
+        while (!(line = br.readLine()).equals("")) {
+            log.debug("line : {}", line);
         }
     }
 

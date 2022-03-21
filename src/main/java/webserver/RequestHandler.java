5dbdedc96ece462bf6ac62a588ff32815d77b6b3
ByeoungJoon Jeon <juni8453@naver.com>
2022-03-21 11:37:35 +0900

    수정) 모든 Request Header 출력하기 구현을 위해 RequestHandler 클래스 수정
    Stream 을 Line 으로 하나하나 읽어서 log 로 찍어보기 위해 run() 메서드를 수정하였습니다.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,8 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,7 +21,16 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+            String headerLine = bufferedReader.readLine();
+
+            log.debug("request header : {}", headerLine);
+
+            while (!headerLine.equals("")) {
+                headerLine = bufferedReader.readLine();
+                log.debug("request header : {}", headerLine);
+            }
+
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);

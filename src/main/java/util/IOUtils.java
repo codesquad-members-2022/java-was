167ae84042942667e670f73bafa5ed40a545a989
nori <nohriter@gmail.com>
2022-03-21 18:09:50 +0900

    refactor: 구현 기능 util 메서드로 분리
    - HttpRequestUtils.getUrl()
    요청받은 리소스 경로를 추출
    - IOUtils.readRequestHeader()
    요청 헤더 정보를 읽어서 리스트로 저장
    - IOUtils.readRequestResource()
    요청받은 경로의 리소스를 읽어들임

--- src/main/java/util/IOUtils.java
+++ src/main/java/util/IOUtils.java
@@ -1,7 +1,13 @@
 package util;
 
+import com.google.common.base.Strings;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import util.HttpRequestUtils.Pair;
 
 public class IOUtils {
     /**
@@ -16,5 +22,18 @@ public class IOUtils {
         char[] body = new char[contentLength];
         br.read(body, 0, contentLength);
         return String.copyValueOf(body);
+    }
+
+    public static List<Pair> readRequestHeader(BufferedReader br) throws IOException {
+        String headLine;
+        List<Pair> headers = new ArrayList<>();
+        while (!Strings.isNullOrEmpty(headLine = br.readLine())) {
+            headers.add(HttpRequestUtils.parseHeader(headLine));
+        }
+        return headers;
+    }
+
+    public static byte[] readRequestResource(String url) throws IOException {
+        return Files.readAllBytes(new File("./webapp" + url).toPath());
     }
 }

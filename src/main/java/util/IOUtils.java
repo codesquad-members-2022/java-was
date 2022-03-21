f22942dde2c2fd523802017b408358090c61fd58
leejohy-0223 <leejohy@naver.com>
2022-03-21 18:45:38 +0900

    feat: 사용자 요청 url에 대한 응답 메서드 구현
    - RequestHandler에서 HTTP Request parsing을 통해 url 추출
    - IOUtils를 통해 url
    파일로부터 byte 코드 추출 후 브라우저로 반환

--- src/main/java/util/IOUtils.java
+++ src/main/java/util/IOUtils.java
@@ -1,9 +1,16 @@
 package util;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class IOUtils {
+
+    private static final String EMPTY_RESULT = "";
+
     /**
      * @param BufferedReader는
      *            Request Body를 시작하는 시점이어야
@@ -16,5 +23,24 @@ public class IOUtils {
         char[] body = new char[contentLength];
         br.read(body, 0, contentLength);
         return String.copyValueOf(body);
+    }
+
+    public static byte[] readFile(String path) throws IOException {
+        File file = new File(path);
+        InputStream inputStream = new FileInputStream(file);
+        return readFromInputStream(inputStream).getBytes();
+    }
+
+    private static String readFromInputStream(InputStream inputStream)
+        throws IOException {
+        StringBuilder resultStringBuilder = new StringBuilder();
+        try (BufferedReader br
+                 = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                resultStringBuilder.append(line).append("\n");
+            }
+        }
+        return resultStringBuilder.toString();
     }
 }
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,18 +1,24 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import util.IOUtils;
+
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
-    private Socket connection;
+    private final Socket connection;
+
+    private static final String staticResourcePath = System.getProperty("user.dir") + "/webapp";
+    private static final int REQUEST_URI_INDEX = 1;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -20,17 +26,38 @@ public class RequestHandler extends Thread {
 
     public void run() {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-                connection.getPort());
+            connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+             OutputStream out = connection.getOutputStream()) {
+
+            // 1. request line 을 읽고 어떤 파일을 요청하는지 파악
+            String requestUrl = readRequestUrl(br);
+
+            // 2. header 를 로그로 출력
+            printHeaders(br);
+
+            // 3. 1번에서 요청한 파일을 읽어서, String -> byte[] 로 변환하여 브라우저로 반환
+            byte[] body = IOUtils.readFile(staticResourcePath + requestUrl);
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private void printHeaders(BufferedReader br) throws IOException {
+        String header;
+        while (!(header = br.readLine()).equals("") && header != null) {
+            log.debug("header : {}", header);
+        }
+    }
+
+    private String readRequestUrl(BufferedReader br) throws IOException {
+        String[] requestLine = br.readLine().split(" ");
+        return requestLine[REQUEST_URI_INDEX];
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {

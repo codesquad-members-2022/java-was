f80ba93baf6458c3fb7383e2ffc5694d12bc3356
WooJin Park <ak2j38@gmail.com>
2022-03-22 12:17:11 +0900

    ♻️HttpClientResquest 리팩토링
    1. 매직리터럴 상수로 분리
    2. 생성자 가독성 향상
    
    end

--- src/main/java/com/riakoader/was/httpmessage/HttpClientRequest.java
+++ src/main/java/com/riakoader/was/httpmessage/HttpClientRequest.java
@@ -1,24 +1,28 @@
 package com.riakoader.was.httpmessage;
 
-
 import com.riakoader.was.util.HttpRequestUtils;
 
 import java.util.List;
+import java.util.Map;
 
 public class HttpClientRequest {
     private static final String REQUEST_LINE_DELIMITER = " ";
+    private static final String QUERYSTRING_DELIMITER = "?";
     private String method;
     private String requestURI;
     private String queryString;
+    private Map<String, String> params;
     private List<HttpRequestUtils.Pair> headers;
 
     public HttpClientRequest(String requestLine, List<HttpRequestUtils.Pair> headers) {
         String[] requestLineTokens = requestLine.split(REQUEST_LINE_DELIMITER);
-        this.method = requestLineTokens[0];
         String requestURL = requestLineTokens[1];
-        int queryStringDelimiterIndex = requestURL.indexOf("?");
+        int queryStringDelimiterIndex = requestURL.indexOf(QUERYSTRING_DELIMITER);
+
+        this.method = requestLineTokens[0];
         this.requestURI = requestURL.substring(0, queryStringDelimiterIndex);
         this.queryString = requestURL.substring(queryStringDelimiterIndex + 1);
+        this.params = HttpRequestUtils.parseQueryString(queryString);
         this.headers = headers;
     }
 
@@ -26,7 +30,7 @@ public class HttpClientRequest {
         return requestURI;
     }
 
-    public String getQueryString() {
-        return queryString;
+    public String getParameter(String name) {
+        return params.get(name);
     }
 }

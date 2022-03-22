c80c85e3efaddd4de4afe5db86c8870f177b1f9a
seyeong <seyoung7555@naver.com>
2022-03-22 14:05:17 +0900

    feat: URL에서 QueryString을 뽑아내는 메소드 추가

--- src/main/java/util/HttpRequestUtils.java
+++ src/main/java/util/HttpRequestUtils.java
@@ -1,16 +1,22 @@
 package util;
 
+import com.google.common.base.Strings;
+import com.google.common.collect.Maps;
+
 import java.util.Arrays;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import com.google.common.base.Strings;
-import com.google.common.collect.Maps;
-
 public class HttpRequestUtils {
+    private HttpRequestUtils() {
+    }
+
+    public static String parseUrl(String url) {
+        return url.split("\\?")[1];
+    }
+
     /**
-     * @param queryString은
-     *            URL에서 ? 이후에 전달되는 field1=value1&field2=value2 형식임
+     * @param queryString은 URL에서 ? 이후에 전달되는 field1=value1&field2=value2 형식임
      * @return
      */
     public static Map<String, String> parseQueryString(String queryString) {
@@ -18,8 +24,7 @@ public class HttpRequestUtils {
     }
 
     /**
-     * @param 쿠키
-     *            값은 name1=value1; name2=value2 형식임
+     * @param 쿠키 값은 name1=value1; name2=value2 형식임
      * @return
      */
     public static Map<String, String> parseCookies(String cookies) {

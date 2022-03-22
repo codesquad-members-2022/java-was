68dbe624732f2af5622cbc5bf91fbb1a4b258423
Minseok-Choi <cms05041@gmail.com>
2022-03-22 11:34:46 +0900

    feat : requestHanlder에 response200withCssHeader 메서드 구현 및 run() 조건문 처리
    .css 파일을 요청했을때,  .css파일이 깨지는 상황때문에 content-type text/css로 처리하는 메서드를 만들었습니다.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -19,20 +19,26 @@ public class RequestHandler extends Thread {
 
     public void run() {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-            connection.getPort());
+        connection.getPort());
 
-            try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-                String headerLine = bufferedReader.readLine();
+        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+            String headerLine = bufferedReader.readLine();
 
-                log.debug("request header : {}", headerLine);
+            log.debug("request header : {}", headerLine);
 
-                String[] tokens = headerLine.split(" ");
-                String url = tokens[1];
+            String[] tokens = headerLine.split(" ");
+            String url = tokens[1];
 
-                DataOutputStream dos = new DataOutputStream(out);
-                byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
-            response200Header(dos, body.length);
+            DataOutputStream dos = new DataOutputStream(out);
+            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+            if(url.endsWith(".css")) {
+                response200withCssHeader(dos, body.length);
+            }
+
+            if(url.endsWith(".html")) {
+                response200Header(dos, body.length);
+            }
             responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
@@ -43,6 +49,17 @@ public class RequestHandler extends Thread {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void response200withCssHeader(DataOutputStream dos, int lengthOfBodyContent) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/css;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {

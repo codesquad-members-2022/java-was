a650bf4ad62e1bec56681a212a4cdbb752bd297a
WooJin Park <ak2j38@gmail.com>
2022-03-22 11:46:38 +0900

    ✨ HttpClientRequest 필드 추가
    1. GET 방식의 회원가입을 지원하기 위해 queryString 필드 추가
    
    end

--- src/main/java/com/riakoader/was/httpmessage/HttpClientRequest.java
+++ src/main/java/com/riakoader/was/httpmessage/HttpClientRequest.java
@@ -6,20 +6,27 @@ import com.riakoader.was.util.HttpRequestUtils;
 import java.util.List;
 
 public class HttpClientRequest {
-
     private static final String REQUEST_LINE_DELIMITER = " ";
     private String method;
     private String requestURI;
+    private String queryString;
     private List<HttpRequestUtils.Pair> headers;
 
     public HttpClientRequest(String requestLine, List<HttpRequestUtils.Pair> headers) {
         String[] requestLineTokens = requestLine.split(REQUEST_LINE_DELIMITER);
         this.method = requestLineTokens[0];
-        this.requestURI = requestLineTokens[1];
+        String requestURL = requestLineTokens[1];
+        int queryStringDelimiterIndex = requestURL.indexOf("?");
+        this.requestURI = requestURL.substring(0, queryStringDelimiterIndex);
+        this.queryString = requestURL.substring(queryStringDelimiterIndex + 1);
         this.headers = headers;
     }
 
     public String getRequestURI() {
         return requestURI;
+    }
+
+    public String getQueryString() {
+        return queryString;
     }
 }

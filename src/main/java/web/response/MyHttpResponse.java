6587aaafcac0ad05054f95f4f3daa60862207275
nak253 <nwon4413@naver.com>
2022-03-23 16:37:19 +0900

    Http 응답을 정의한 MyHttpResponse 클래스 정의
    - redirectTo : 지정된 redirectPath로 리다이렉트하는 Http메시지를 작성해주는 메서드
    - responseStaticResource : 지정된 path의 정적리소스를 응답하는 Http메시지를 작성해주는 메서드

---
+++ src/main/java/web/response/MyHttpResponse.java
@@ -0,0 +1,32 @@
+package web.response;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+
+public class MyHttpResponse {
+
+    private DataOutputStream dos;
+    private byte[] body;
+
+    public MyHttpResponse(OutputStream out) {
+        dos = new DataOutputStream(out);
+    }
+
+    public void redirectTo(String redirectPath) throws IOException {
+        dos.writeBytes("HTTP/1.1 302 Found \r\n");
+        dos.writeBytes("location: " + redirectPath + "\r\n");
+    }
+
+    public void responseStaticResource(String path) throws IOException {
+        body = Files.readAllBytes(new File("./webapp" + path).toPath());
+        dos.writeBytes("HTTP/1.1 200 OK \r\n");
+        dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+        dos.writeBytes("Content-Length: " + body.length + "\r\n");
+        dos.writeBytes("\r\n");
+        dos.write(body, 0, body.length);
+        dos.flush();
+    }
+}
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -5,10 +5,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import web.request.MyHttpMethod;
 import web.request.MyHttpRequest;
+import web.response.MyHttpResponse;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
-import java.nio.file.Files;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -25,56 +27,25 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             MyHttpRequest request = new MyHttpRequest(in);
-            sendResponse(request, out);
+            MyHttpResponse response = new MyHttpResponse(out);
+            sendResponse(request, response);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
 
-    private void sendResponse(MyHttpRequest request, OutputStream out) throws IOException {
+    private void sendResponse(MyHttpRequest request, MyHttpResponse response) throws IOException {
         String path = request.getPath();
         MyHttpMethod method = request.getMethod();
-        DataOutputStream dos = new DataOutputStream(out);
-        byte[] body;
-
         if (path.equals("/user/create") && method.isPost()) {
             User user = new User(request.getParameter("userId"),
                     request.getParameter("password"),
                     request.getParameter("name"),
                     request.getParameter("email"));
             log.debug("user created : {}", user);
-            setResponseCode302(dos);
-            setRedirectLocation(dos, "/index.html");
-            dos.flush();
+            response.redirectTo("/index.html");
             return;
         }
-        body = Files.readAllBytes(new File("./webapp" + path).toPath());
-        setResponseCode200(dos);
-        setResponseHtml(dos, body.length);
-        responseBody(dos, body);
+        response.responseStaticResource(path);
     }
-
-    private void setRedirectLocation(DataOutputStream dos, String redirectLocation) throws IOException {
-        dos.writeBytes("location: " + redirectLocation + "\r\n");
-    }
-
-    private void setResponseCode200(DataOutputStream dos) throws IOException {
-        dos.writeBytes("HTTP/1.1 200 OK \r\n");
-    }
-
-    private void setResponseCode302(DataOutputStream dos) throws IOException {
-        dos.writeBytes("HTTP/1.1 302 Found \r\n");
-    }
-
-    private void setResponseHtml(DataOutputStream dos, int lengthOfBodyContent) throws IOException {
-        dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-        dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-        dos.writeBytes("\r\n");
-    }
-
-    private void responseBody(DataOutputStream dos, byte[] body) throws IOException {
-        dos.write(body, 0, body.length);
-        dos.flush();
-    }
-
 }

e4a4519050df90782a1ee46a0463b6b9970f6686
WooJin Park <ak2j38@gmail.com>
2022-03-23 19:18:21 +0900

    ✨ HandlerMethod 관련 기능 구현
    1. 다양한 요청 처리를 위한 HandlerMethod라는 함수형 인터페이스 추가
    2. 인터페이스 구현을 위한 mapper클래스 추가
    
    end

---
+++ src/main/java/com/riakoader/was/handler/HandlerMethodMapper.java
@@ -0,0 +1,57 @@
+package com.riakoader.was.handler;
+
+import com.riakoader.was.db.DataBase;
+import com.riakoader.was.httpmessage.HttpResponse;
+import com.riakoader.was.httpmessage.HttpStatus;
+import com.riakoader.was.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileReader;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+public class HandlerMethodMapper {
+
+    private static final Logger logger = LoggerFactory.getLogger(HandlerMethodMapper.class);
+
+    private static final Map<String, HandlerMethod> mapper = new HashMap<>();
+
+    private static final HandlerMethod resourceHandlerMethod = (request) -> {
+        HttpResponse response = new HttpResponse();
+        response.setStatusLine(request.getProtocol(), HttpStatus.OK);
+        response.setHeader("Content-Type", "text/html;charset=utf-8");
+
+        FileReader resources= new FileReader("src/main/resources/env.properties");
+        Properties properties = new Properties();
+        properties.load(resources);
+
+        byte[] body = Files.readAllBytes(new File(properties.getProperty("webapp_path") + request.getRequestURI()).toPath());
+        response.setHeader("Content-Length", Integer.toString(body.length));
+        response.setBody(body);
+
+        return response;
+    };
+
+    static {
+        mapper.put("/user/create", (request) -> {
+            User user = new User(request.getParameter("userId"), request.getParameter("password"),
+                request.getParameter("name"), request.getParameter("email"));
+
+            DataBase.addUser(user);
+            logger.debug("user: {}", DataBase.findAll());
+
+            HttpResponse response = new HttpResponse();
+            response.setStatusLine(request.getProtocol(), HttpStatus.OK);
+
+            return response;
+        });
+    }
+
+    public static HandlerMethod getHandlerMethod(String uri) {
+        return mapper.getOrDefault(uri, resourceHandlerMethod);
+    }
+}

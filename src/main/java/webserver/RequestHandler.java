386734443fd438ed4557cb9b0e5c0bb6021017e2
함석호 <kalejuice02@gmail.com>
2022-03-23 16:55:01 +0900

    refactor : 기존 get 회원가입 요청을 처리하는 코드 -> post 회원가입 요청을 처리하도록 코드 수정
    - getResponse, postResponse 메서드를 추가
    - if문의 조건을 httpMethod로 변경
    - 기존 header를 생성하는 메서드 제거

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,15 +1,13 @@
 package webserver;
 
+import model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.util.Map;
-
-import model.User;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import util.HttpRequestUtils;
-import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -25,45 +23,43 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(in));
-            Map<String, String> requestHeader = IOUtils.readRequestHeader(br);
-            String path = requestHeader.get("path");
-
-            if (path.contains("create")) {
-                Map<String, String> queryString = HttpRequestUtils.parseQueryString(path.split("\\?")[1]);
-                User user = new User(queryString.get("userId"), queryString.get("password"), queryString.get("name"), queryString.get("email"));
-                log.debug("{}",user);
-                path = "/index.html";
-            }
-            
-            byte[] body = Files.readAllBytes(new File("./webapp" + path).toPath());
-
             DataOutputStream dos = new DataOutputStream(out);
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            HttpRequest request = new HttpRequest(br);
+
+            if (request.getHttpMethod().equals("POST")) {
+                Map<String, String> userInfo = request.getParams();
+                User user = new User(userInfo.get("userId"), userInfo.get("password"), userInfo.get("name"), userInfo.get("email"));
+                log.debug("{}", user);
+
+                sendPostResponse(dos);
+            } else {
+                sendGetResponse(new DataOutputStream(dos), request.getAbsPath());
+            }
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private void sendPostResponse(DataOutputStream dos) {
+        HttpResponse response = new HttpResponse(302, dos);
         try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
+            response.writeLocation("/index.html");
+            response.send();
         } catch (IOException e) {
-            log.error(e.getMessage());
+            e.printStackTrace();
         }
     }
 
-    private void responseBody(DataOutputStream dos, byte[] body) {
+    private void sendGetResponse(DataOutputStream dos, String path) {
         try {
-            dos.write(body, 0, body.length);
-            dos.flush();
+            byte[] body = Files.readAllBytes(new File("./webapp" + path).toPath());
+            HttpResponse response = new HttpResponse(200, dos);
+            response.writeBody(body);
+            response.send();
         } catch (IOException e) {
-            log.error(e.getMessage());
+            e.printStackTrace();
         }
     }
+
 }

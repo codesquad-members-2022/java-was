bb8ee7116d7fb919a0d7c22ce7de083ba19e35b9
leejohy-0223 <leejohy@naver.com>
2022-03-23 12:41:24 +0900

    feat: 동적 요청을 처리할 서블릿 클래스를 런타임에 생성하고 호출해주는 Dispatcher 클래스 추가

---
+++ src/main/java/webserver/Dispatcher.java
@@ -0,0 +1,40 @@
+package webserver;
+
+import java.lang.reflect.Constructor;
+
+import config.RequestMapping;
+import http.HttpServlet;
+import http.Request;
+import http.Response;
+import webserver.dto.RequestLine;
+
+public class Dispatcher {
+
+    private static Dispatcher instance;
+
+    private Dispatcher() {
+    }
+
+    public synchronized static Dispatcher getInstance() {
+        if (instance == null) {
+            instance = new Dispatcher();
+        }
+        return instance;
+    }
+
+    public Response handleRequest(RequestLine requestLine) throws Exception {
+        Class<? extends HttpServlet> servletClass = RequestMapping.findHandlerMethod(requestLine.getHttpMethod(),
+                requestLine.getUrl())
+            .orElseThrow();
+
+        Request request = Request.of(requestLine);
+        Response response = new Response();
+
+        Constructor<? extends HttpServlet> constructor = servletClass.getConstructor(Request.class, Response.class);
+        HttpServlet httpServlet = constructor.newInstance(request, response);
+
+        return httpServlet.service();
+    }
+
+}
+

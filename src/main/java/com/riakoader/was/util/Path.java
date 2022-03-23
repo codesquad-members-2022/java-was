557baae0b4344ee6c9f9d6388f5a88bf0300c39b
WooJin Park <ak2j38@gmail.com>
2022-03-23 19:17:19 +0900

    🔧 설정 파일 추가
    1. webapp의 경로 및 추후 관리될 경로들을 저장하는 properties 파일 추가
    2. Path 클래스 삭제
    
    end

--- src/main/java/com/riakoader/was/util/Path.java
+++
@@ -1,8 +0,0 @@
-package com.riakoader.was.util;
-
-import static java.io.File.separatorChar;
-
-public class Path {
-
-    public static final String WEBAPP_PATH = "." + separatorChar + "webapp" + separatorChar;
-}

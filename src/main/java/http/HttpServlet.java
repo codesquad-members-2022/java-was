bc575cf7d8893a8349fd0dc5e3877162c98f57f0
leejohy-0223 <leejohy@naver.com>
2022-03-23 12:38:46 +0900

    feat: HTTP 요청 및 응답 처리를 담당하는 템플릿 추상 클래스 생성
    - 비즈니스 로직을 담당하는 서블릿 객체 추상화

---
+++ src/main/java/http/HttpServlet.java
@@ -0,0 +1,32 @@
+package http;
+
+public abstract class HttpServlet {
+
+    protected Request request;
+    protected Response response;
+
+    protected HttpServlet(Request request, Response response) {
+        this.request = request;
+        this.response = response;
+    }
+
+    public Response service() {
+        switch (request.getHttpMethod()) {
+            case GET:
+                return doGet();
+            case POST:
+                return doPost();
+        }
+
+        throw new IllegalStateException("Unsupported http method");
+    }
+
+    public Response doGet() {
+        throw new IllegalStateException("Unimplemented get method");
+    }
+
+    public Response doPost() {
+        throw new IllegalStateException("Unimplemented post method");
+    }
+
+}

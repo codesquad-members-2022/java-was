5d593564f4667eb21210621e47562504fa918df1
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-03-25 01:43:26 +0900

    웹서버 2 단계 - GET으로 회원가입 기능 구현 시작
    Contents
    - HEADER 타입 추가
    
    Todo
    - 2단계 README 작성

--- src/main/java/model/HeaderType.java
+++ src/main/java/model/HeaderType.java
@@ -1,12 +1,36 @@
 package model;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public enum HeaderType {
 
     CONTENT_TYPE("Content-Type"),
     CONTENT_LENGTH("Content-Length"),
+    REQUEST_METHOD("Request Method"),
+    REQUEST_URL("Request URL"),
+    CACHE_CONTROL("Cache-Control"),
+    PORT("Port"),
+    CONNECTION("Connection"),
+    SEC_CH_UA_PLATFORM("sec-ch-ua-platform"),
+    SEC_FETCH_MODE("Sec-Fetch-Mode"),
+    SEC_CH_UA_MOBILE("sec-ch-ua-mobile"),
+    SEC_FETCH_USER("Sec-Fetch-User"),
+    SEC_CH_UA("sec-ch-ua"),
+    USER_AGENT("User-Agent"),
+    PURPOSE("Purpose"),
+    SEC_FETCH_DEST("Sec-Fetch-Dest"),
+    HOST("Host"),
+    ACCEPT("Accept"),
+    SEC_FETCH_SITE("Sec-Fetch-Site"),
+    ACCEPT_ENCODING("Accept-Encoding"),
+    UPGRADE_INSECURE_REQUESTS("Upgrade-Insecure-Requests"),
+    COOKIE("Cookie"),
+    ACCEPT_LANGUAGE("Accept-Language"),
+    REFERER("Referer"),
+    ORIGIN("Origin"),
     NONE("");
 
     private final String value;
@@ -15,12 +39,17 @@ public enum HeaderType {
         this.value = value;
     }
 
-    public static String of(String inputType) {
+    public static HeaderType of(String inputType) {
         return Arrays.stream(values())
                 .filter(headerType -> euqalTo(headerType.value, inputType))
-                .map(HeaderType::getValue)
                 .findAny()
-                .orElseGet(none());
+                .orElse(NONE);
+    }
+
+    public static List<String> getHeaderTypes() {
+        return Arrays.stream(values())
+                .map(HeaderType::getValue)
+                .collect(Collectors.toUnmodifiableList());
     }
 
     private static boolean euqalTo(String headerType, String inputType) {
@@ -29,9 +58,5 @@ public enum HeaderType {
 
     public String getValue() {
         return value;
-    }
-
-    private static Supplier<String> none() {
-        return NONE::getValue;
     }
 }

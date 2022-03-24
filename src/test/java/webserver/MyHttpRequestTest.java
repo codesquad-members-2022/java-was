20f1280a6df39b5c8ddd2a9772f11458976f6d5c
dukcode <duk9741@gmail.com>
2022-03-24 15:50:39 +0900

    Test: MyHttpRequest 헤더 초기화 테스트

--- src/test/java/webserver/MyHttpRequestTest.java
+++ src/test/java/webserver/MyHttpRequestTest.java
@@ -1,6 +1,7 @@
 package webserver;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
@@ -13,17 +14,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MyHttpRequestTest {
 
+    String requestMessage = "GET /index.html&userId=test&password=hello HTTP/1.1\n"
+            + "Host: localhost:8080\n"
+            + "Connection: keep-alive\n"
+            + "Accept: text/html,*/*\n";
+
+    InputStream is = new ByteArrayInputStream(requestMessage.getBytes());
+    MyHttpRequest request = new MyHttpRequest(is);
+
     @Test
-    void headerTest() {
-        String str = "GET /index.html&userId=test&password=hello HTTP/1.1\n"
-                + "Host: localhost:8080\n"
-                + "Connection: keep-alive\n"
-                + "Accept: */*";
-
-        // convert String into InputStream
-        InputStream is = new ByteArrayInputStream(str.getBytes());
-
-        MyHttpRequest request = new MyHttpRequest(is);
+    void startLineTest() {
         Map<String, String> paramMap = new HashMap<String, String>();
         paramMap.put("userId", "test");
         paramMap.put("password", "hello");
@@ -32,6 +32,19 @@ class MyHttpRequestTest {
         assertThat(request.getRequestURI()).isEqualTo("/index.html");
         assertThat(request.getParamMap()).isEqualTo(paramMap);
         assertThat(request.getProtocol()).isEqualTo("HTTP/1.1");
+    }
+
+    @Test
+    void headerTest() {
+
+        String[] hostValues = {"localhost:8080"};
+        String[] connectionValues = {"keep-alive"};
+        String[] acceptValues = {"text/html", "*/*"};
+
+
+        assertThat(request.getHeader("Host")).isEqualTo(hostValues);
+        assertThat(request.getHeader("Connection")).isEqualTo(connectionValues);
+        assertThat(request.getHeader("Accept")).isEqualTo(acceptValues);
     }
 
 }

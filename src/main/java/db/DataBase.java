4154ed757cc04598c9797d397c719d50771cb5e1
함석호 <kalejuice02@gmail.com>
2022-03-25 11:24:34 +0900

    refactor : 중복된 아이디가 존재하지 않을경우에만 저장되도록 코드 수정

--- src/main/java/db/DataBase.java
+++ src/main/java/db/DataBase.java
@@ -1,19 +1,18 @@
 package db;
 
+import model.User;
+
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-
-import com.google.common.collect.Maps;
-
-import model.User;
 
 public class DataBase {
     private static Map<String, User> users = new HashMap<>();
 
     public static void addUser(User user) {
-        checkDuplicate(user.getUserId());
-        users.put(user.getUserId(), user);
+        if(!checkDuplicate(user.getUserId())){
+            users.put(user.getUserId(), user);
+        }
     }
 
     public static User findUserById(String userId) {
@@ -24,10 +23,8 @@ public class DataBase {
         return users.values();
     }
 
-    private static void checkDuplicate(String userId) {
-        if (users.containsKey(userId)) {
-            throw new IllegalArgumentException("중복된 userId입니다.");
-        }
+    private static boolean checkDuplicate(String userId) {
+        return users.containsKey(userId);
     }
 
 }

b9e08c1dff4d85f72931d4f029f21d584c338b63
bugpigg <bugpigg@gmail.com>
2022-03-25 18:07:31 +0900

    feat : request line 에서 메소드 방식을 추출하는 메서드 추가
    - RequestLineUtil 에 request line 으로 부터 메소드 방식을 추출하는 메서드를 추가하였습니다.

--- src/main/java/util/RequestLineUtil.java
+++ src/main/java/util/RequestLineUtil.java
@@ -2,11 +2,17 @@ package util;
 
 public class RequestLineUtil {
 
+    public static final int METHOD_INDEX = 0;
     public static final int URL_INDEX = 1;
 
     public static String getURL(String requestLine) {
         String[] strList = requestLine.split(" ");
         return strList[URL_INDEX];
+    }
+
+    public static String getMethodType(String requestLine) {
+        String[] strList = requestLine.split(" ");
+        return strList[METHOD_INDEX];
     }
 
 }

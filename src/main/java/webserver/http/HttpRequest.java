ff72f410274e67c7d1bd782da816424def2c417f
seyeong <seyoung7555@naver.com>
2022-03-25 10:59:35 +0900

    feat(HttpRequest): 요청 정보를 담고 있는 HttpRequest 객체 생성
    - Http Method, url, version, requestHeader, body를 저장
    - QueryString을 분리하는 책임 수행

---
+++ src/main/java/webserver/http/HttpRequest.java
@@ -0,0 +1,60 @@
+package webserver.http;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpRequest {
+    private static final Logger log = LoggerFactory.getLogger(HttpRequest.class);
+    private final String method;
+    private final String url;
+    private final String version;
+    private final Map<String, String> requestHeader;
+    private String body;
+    private Map<String, String> params = new HashMap<>();
+
+    public HttpRequest(String method, String url, String version, Map<String, String> requestHeader, String body) {
+        this.method = method;
+        if (url.contains("?")) {
+            this.url = url.split("\\?")[0];
+            String queryString = HttpRequestUtils.parseUrl(url);
+            params = HttpRequestUtils.parseQueryString(queryString);
+        } else {
+            this.url = url;
+        }
+        this.version = version;
+        this.requestHeader = requestHeader;
+        this.body = body;
+        params.putAll(HttpRequestUtils.parseQueryString(body));
+
+        log.debug("params: {}", params);
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public Map<String, String> getRequestHeader() {
+        return requestHeader;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public String getParameter(String parameterName) {
+        return params.get(parameterName);
+    }
+
+}

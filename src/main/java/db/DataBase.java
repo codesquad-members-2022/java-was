083798e7692c3d2c796fe6b9ab5881f0743784ba
함석호 <kalejuice02@gmail.com>
2022-03-25 11:17:26 +0900

    feat : 중복된 이름이 입력될 경우 예외를 던지도록 checkDuplicate 메서드 작성

--- src/main/java/db/DataBase.java
+++ src/main/java/db/DataBase.java
@@ -1,6 +1,7 @@
 package db;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 
 import com.google.common.collect.Maps;
@@ -8,9 +9,10 @@ import com.google.common.collect.Maps;
 import model.User;
 
 public class DataBase {
-    private static Map<String, User> users = Maps.newHashMap();
+    private static Map<String, User> users = new HashMap<>();
 
     public static void addUser(User user) {
+        checkDuplicate(user.getUserId());
         users.put(user.getUserId(), user);
     }
 
@@ -21,4 +23,11 @@ public class DataBase {
     public static Collection<User> findAll() {
         return users.values();
     }
+
+    private static void checkDuplicate(String userId) {
+        if (users.containsKey(userId)) {
+            throw new IllegalArgumentException("중복된 userId입니다.");
+        }
+    }
+
 }

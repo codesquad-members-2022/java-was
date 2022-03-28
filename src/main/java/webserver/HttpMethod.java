e27b1377917e2cbb6dfb4f2728438e8c8febf4b7
jun <devjun1023@gmail.com>
2022-03-28 15:22:37 +0900

    HttpMethod 구현
    Contents
    - HttpMethod 구현
    
    Todo
    - HttpHeader 구현

--- src/main/java/webserver/HttpMethod.java
+++ src/main/java/webserver/HttpMethod.java
@@ -1,8 +1,23 @@
 package webserver;
 
-public class HttpMethod {
+import java.util.HashMap;
+import java.util.Map;
+
+public enum HttpMethod {
+
+    GET,
+    POST;
+
+    private static final Map<String, HttpMethod> mappings = new HashMap<>(2);
+
+    static {
+        for (HttpMethod httpMethod : values()) {
+            System.out.println(httpMethod.name());
+            mappings.put(httpMethod.name(), httpMethod);
+        }
+    }
 
     public static HttpMethod getMethod(String methodType) {
-        return null;
+        return (methodType != null ? mappings.get(methodType) : null);
     }
 }

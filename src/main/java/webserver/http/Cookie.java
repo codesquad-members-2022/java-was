5ce450719b0efceaa04d4b7b65e57284508711c2
leezzangmin <ckdals12345678@gmail.com>
2022-03-28 16:11:39 +0900

    feat: 쿠키를 만료시키는 expire() 메서드 추가

--- src/main/java/webserver/http/Cookie.java
+++ src/main/java/webserver/http/Cookie.java
@@ -1,36 +1,42 @@
 package webserver.http;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import model.User;
 
 public class Cookie {
-    private String name;
-    private String value;
-    private String path;
-    private String expire;
 
-    public Cookie(String name, String value) {
-        this.name = name;
-        this.value = value;
+    private final static String DIVIDER = "; ";
+
+    private String userId;
+    private String path;
+    private String expireDateTime;
+
+    public Cookie(User user) {
+        this.userId = user.getUserId();
+        this.path = "path=/; ";
+        generateExpireDate();
     }
 
-    public String getCookieString() {
+    public String getCookieString(String sessionId) {
         StringBuilder sb = new StringBuilder();
-        sb.append(name);
-        sb.append("=");
-        sb.append(value);
-        sb.append("; ");
-        sb.append("path=/");
-//        sb.append(getExpireDate());
+        sb.append("sessionId="+sessionId);
+        sb.append(DIVIDER);
+        sb.append(path);
+        sb.append(expireDateTime);
 
         return sb.toString();
     }
 
-    private String getExpireDate() {
-        String date = LocalDateTime.now().plusHours(1)
-                .format(DateTimeFormatter.ofPattern(String.valueOf(DateTimeFormatter.RFC_1123_DATE_TIME)));
-        return "expires=" + date + ";";
+    public void expire() {
+        this.expireDateTime = "Expires=Mon, 28-Mar-1970 06:41:18 GMT;";
     }
+
+    private void generateExpireDate() {
+        this.expireDateTime = "Expires=Mon, 28-Mar-2025 06:41:18 GMT;";
+//                LocalDateTime.now().plusHours(1)
+//                .format(DateTimeFormatter.ofPattern(String.valueOf(DateTimeFormatter.RFC_1123_DATE_TIME)));
+//        return "Expires=" + expireDateTime + DIVIDER;
+    }
+
 
 
 }

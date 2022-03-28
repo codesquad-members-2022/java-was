ff4dbae0d7f7a730173da0baa755bd03583d7008
hsjang <hsjang0405@gmail.com>
2022-03-28 14:18:10 +0900

    feat: 로그인 유저 확인하고 response에 Set-Cookie 헤더 작성

--- src/main/java/webserver/Response.java
+++ src/main/java/webserver/Response.java
@@ -12,8 +12,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Response {
+
 	private Logger log = LoggerFactory.getLogger(Response.class);
-	private byte[] body;
+	private byte[] body = new byte[0];
 	private DataOutputStream dos;
 	private Request request;
 
@@ -24,9 +25,8 @@ public class Response {
 
 	public void writeResponse() throws IOException {
 		log.debug("requestLine: {}", request.getRequestLine());
-		if (HttpMethod.isPost(request) && request.getPath().equals("/user/create")) {
 
-			this.body = Files.readAllBytes(new File("./webapp/index.html").toPath());
+		if (HttpMethod.isPost(request) && request.getPath().equals("/user/create")) {
 
 			Map<String, String> parsedBody = request.takeParsedBody();
 			log.debug("POST BODY: {}", parsedBody);
@@ -39,13 +39,31 @@ public class Response {
 			saveUser(user);
 			return;
 		}
+
+		if (HttpMethod.isPost(request) && request.getPath().equals("/user/login")) {
+			Map<String, String> parsedBody = request.takeParsedBody();
+			log.debug("POST BODY: {}", parsedBody);
+
+			User user = DataBase.findUserById(parsedBody.get("userId"));
+
+			if (user != null && user.getPassword().equals(parsedBody.get("password"))) {
+				response302Header("http://localhost:8080/index.html", user.getUserId());
+				responseBody();
+				return;
+			}
+			response302Header("http://localhost:8080/user/login_failed.html");
+			responseBody();
+			return;
+		}
+
+		//GET 일 때
 		this.body = Files.readAllBytes(new File("./webapp" + request.getPath()).toPath());
 		response200Header();
 		responseBody();
 	}
 
 	private void saveUser(User user) {
-		if (DataBase.validateDuplicatedId(user)){
+		if (DataBase.validateDuplicatedId(user)) {
 			DataBase.addUser(user);
 			response302Header("http://localhost:8080/index.html");
 			responseBody();
@@ -53,7 +71,6 @@ public class Response {
 		}
 		response302Header("http://localhost:8080/user/form.html");
 		responseBody();
-		return;
 	}
 
 	private void response302Header(String redirectURL) {
@@ -62,6 +79,19 @@ public class Response {
 			dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
 			dos.writeBytes("Content-Length: " + body.length + "\r\n");
 			dos.writeBytes("Location: " + redirectURL + "\r\n");
+			dos.writeBytes("\r\n");
+		} catch (IOException e) {
+			log.error(e.getMessage());
+		}
+	}
+
+	private void response302Header(String redirectURL, String userId) {
+		try {
+			dos.writeBytes("HTTP/1.1 302 Found\r\n");
+			dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+			dos.writeBytes("Content-Length: " + body.length + "\r\n");
+			dos.writeBytes("Location: " + redirectURL + "\r\n");
+			dos.writeBytes("Set-Cookie: sessionId=" + userId + "; Path=/");
 			dos.writeBytes("\r\n");
 		} catch (IOException e) {
 			log.error(e.getMessage());

f13d3cf5335add9f3fc1efa9b7be2d72a7b78bdc
jun <devjun1023@gmail.com>
2022-03-28 15:25:21 +0900

    HttpHeader 구현
    Contents
    - HttpHeader 로직 이동.
    - 디온 요구사항 반영
    
    Todo
    - HttpRequest get메서드 수정

---
+++ src/main/java/webserver/HttpHeaders.java
@@ -0,0 +1,27 @@
+package webserver;
+
+import model.HeaderType;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class HttpHeaders {
+
+    private static final Map<String, HeaderType> headerTypeMap;
+
+    static {
+        headerTypeMap = getHeaderType();
+    }
+
+    public static Map<String, HeaderType> getHeaderType() {
+        Map<String, HeaderType> headerMap = new ConcurrentHashMap<>();
+        for (String headerType : HeaderType.getHeaderTypes()) {
+            headerMap.put(headerType, HeaderType.of(headerType));
+        }
+        return headerMap;
+    }
+
+    public HeaderType getHeaderType(String type) {
+        return headerTypeMap.get(type);
+    }
+}

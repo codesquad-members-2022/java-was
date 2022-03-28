1c4cdfbee0cb58031df36e8e653d91d73409846d
naneun <rmtlrksdp@naver.com>
2022-03-28 21:57:58 +0900

    ♻️WebServerContext 클래스의 HandlerMethod 타입 필드들 삭제
    1. WebServerConfig 클래스 addHandlerMethod 에서 직접 람다식으로 추가하는 방식으로 변경
    
    end
    
    Co-authored-by: ak2j38 <ak2j38@gmail.com>

--- src/main/java/com/riakoader/was/config/WebServerConfig.java
+++ src/main/java/com/riakoader/was/config/WebServerConfig.java
@@ -1,13 +1,22 @@
 package com.riakoader.was.config;
 
+import com.riakoader.was.db.DataBase;
 import com.riakoader.was.handler.HandlerMethodMapper;
 import com.riakoader.was.httpmessage.HttpMethod;
+import com.riakoader.was.httpmessage.HttpResponse;
+import com.riakoader.was.httpmessage.HttpStatus;
+import com.riakoader.was.model.User;
 import com.riakoader.was.util.Pair;
 import com.riakoader.was.webserver.WebServerContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Files;
+import java.util.Properties;
 
 public class WebServerConfig implements WebServerConfigurer {
 
@@ -18,7 +27,7 @@ public class WebServerConfig implements WebServerConfigurer {
     private final WebServerContext webServerContext;
 
     private WebServerConfig() throws NoSuchFieldException, ClassNotFoundException, InvocationTargetException,
-            NoSuchMethodException, InstantiationException, IllegalAccessException {
+            NoSuchMethodException, IllegalAccessException {
 
         logger.debug("WebServerConfig() start");
 
@@ -42,9 +51,77 @@ public class WebServerConfig implements WebServerConfigurer {
     public void addHandlerMethod(HandlerMethodRegistry handlerMethodRegistry) {
         logger.debug("addHandlerMethod() start");
 
-        handlerMethodRegistry.addHandlerMethod(webServerContext.resourceHandlerMethod);
-        handlerMethodRegistry.addHandlerMethod(webServerContext.joinHandlerMethodForGet);
-        handlerMethodRegistry.addHandlerMethod(webServerContext.joinHandlerMethodForPost);
+        handlerMethodRegistry.addHandlerMethod((request) -> {
+
+            HttpResponse response = new HttpResponse(request.getProtocol());
+
+            FileReader resources = null;
+            try {
+                resources = new FileReader("src/main/resources/env.properties");
+            } catch (FileNotFoundException e) {
+                logger.error(e.getMessage());
+                response.setStatus(HttpStatus.NOT_FOUND);
+                return response;
+            }
+
+            Properties properties = new Properties();
+            properties.load(resources);
+
+            byte[] body = Files.readAllBytes(
+                    new File(properties.getProperty("webapp_path") + request.getRequestURI()).toPath()
+            );
+
+            response.setStatus(HttpStatus.OK);
+            response.setHeader("Content-Length", Integer.toString(body.length));
+            response.setBody(body);
+
+            return response;
+        });
+
+        handlerMethodRegistry.addHandlerMethod((request) -> {
+
+            User user = new User(
+                    request.getParameter("userId"),
+                    request.getParameter("password"),
+                    request.getParameter("name"),
+                    request.getParameter("email")
+            );
+
+            DataBase.addUser(user);
+            logger.debug("user: {}", DataBase.findAll());
+
+            HttpResponse response = new HttpResponse(request.getProtocol());
+            response.setStatus(HttpStatus.CREATED);
+
+            return response;
+        });
+
+        handlerMethodRegistry.addHandlerMethod((request) -> {
+
+            if (DataBase.findUserById(request.getParameter("userId")) != null) {
+                HttpResponse response = new HttpResponse(request.getProtocol());
+                response.setStatus(HttpStatus.FOUND);
+                response.setHeader("Location", "/user/form.html");
+
+                return response;
+            }
+
+            User user = new User(
+                    request.getParameter("userId"),
+                    request.getParameter("password"),
+                    request.getParameter("name"),
+                    request.getParameter("email")
+            );
+
+            DataBase.addUser(user);
+            logger.debug("user: {}", DataBase.findAll());
+
+            HttpResponse response = new HttpResponse(request.getProtocol());
+            response.setStatus(HttpStatus.FOUND);
+            response.setHeader("Location", "/index.html");
+
+            return response;
+        });
 
         logger.debug("addHandlerMethod() end");
     }
--- src/main/java/com/riakoader/was/webserver/WebServerContext.java
+++ src/main/java/com/riakoader/was/webserver/WebServerContext.java
@@ -1,27 +1,13 @@
 package com.riakoader.was.webserver;
 
 import com.riakoader.was.config.HandlerMethodRegistry;
-import com.riakoader.was.db.DataBase;
-import com.riakoader.was.handler.HandlerMethod;
 import com.riakoader.was.handler.HandlerMethodMapper;
-import com.riakoader.was.httpmessage.HttpResponse;
-import com.riakoader.was.httpmessage.HttpStatus;
-import com.riakoader.was.model.User;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.nio.file.Files;
-import java.util.Properties;
 
 public class WebServerContext {
-
-    private static final Logger logger = LoggerFactory.getLogger(WebServerContext.class);
 
     private static volatile WebServerContext webServerContext;
 
@@ -54,79 +40,4 @@ public class WebServerContext {
 
         return method.invoke(null);
     }
-
-    public HandlerMethod resourceHandlerMethod =
-            (request) -> {
-
-                HttpResponse response = new HttpResponse(request.getProtocol());
-
-                FileReader resources = null;
-                try {
-                    resources = new FileReader("src/main/resources/env.properties");
-                } catch (FileNotFoundException e) {
-                    logger.error(e.getMessage());
-                    response.setStatus(HttpStatus.NOT_FOUND);
-                    return response;
-                }
-
-                Properties properties = new Properties();
-                properties.load(resources);
-
-                byte[] body = Files.readAllBytes(
-                        new File(properties.getProperty("webapp_path") + request.getRequestURI()).toPath()
-                );
-
-                response.setStatus(HttpStatus.OK);
-                response.setHeader("Content-Length", Integer.toString(body.length));
-                response.setBody(body);
-
-                return response;
-            };
-
-    public HandlerMethod joinHandlerMethodForGet =
-            (request) -> {
-
-                User user = new User(
-                        request.getParameter("userId"),
-                        request.getParameter("password"),
-                        request.getParameter("name"),
-                        request.getParameter("email")
-                );
-
-                DataBase.addUser(user);
-                logger.debug("user: {}", DataBase.findAll());
-
-                HttpResponse response = new HttpResponse(request.getProtocol());
-                response.setStatus(HttpStatus.CREATED);
-
-                return response;
-            };
-
-    public HandlerMethod joinHandlerMethodForPost =
-            (request) -> {
-
-                if (DataBase.findUserById(request.getParameter("userId")) != null) {
-                    HttpResponse response = new HttpResponse(request.getProtocol());
-                    response.setStatus(HttpStatus.FOUND);
-                    response.setHeader("Location", "/user/form.html");
-
-                    return response;
-                }
-
-                User user = new User(
-                        request.getParameter("userId"),
-                        request.getParameter("password"),
-                        request.getParameter("name"),
-                        request.getParameter("email")
-                );
-
-                DataBase.addUser(user);
-                logger.debug("user: {}", DataBase.findAll());
-
-                HttpResponse response = new HttpResponse(request.getProtocol());
-                response.setStatus(HttpStatus.FOUND);
-                response.setHeader("Location", "/index.html");
-
-                return response;
-            };
 }

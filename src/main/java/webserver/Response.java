fbc92069ee8af62e3c9acc8f3734571c18b468ea
bugpigg <bugpigg@gmail.com>
2022-03-28 18:10:21 +0900

    refact : Response 클래스 관련 코멘트 반영
    - 기존의 필드로 관리되었던 DataOutputStream 을 제거하였습니다.
    - 출력 부분을 RequestHandler 가 담당하게 수정하였습니다.

--- src/main/java/webserver/Response.java
+++ src/main/java/webserver/Response.java
@@ -1,65 +1,55 @@
 package webserver;
 
-import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Response {
 
-    private static final Logger log = LoggerFactory.getLogger(Response.class);
-
+    private byte[] body;
+    private String headers;
     private String requestLine;
     private URL url;
-    private DataOutputStream dos;
 
-    public Response(String requestLine, URL url, DataOutputStream dos) {
+    public Response(String requestLine, URL url) {
         this.requestLine = requestLine;
         this.url = url;
-        this.dos = dos;
     }
 
     public void action() throws IOException {
-        if (requestLine.contains("POST") && url.comparePath("/user/create")) {
-            response302Header(url, dos);
+        if (requestLine.contains("POST") && requestLine.contains("/user/create")) {
+            headers = response302Header(url);
         } else {
-            byte[] body = Files.readAllBytes(new File("./webapp" + url.getPath()).toPath());
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            body = Files.readAllBytes(new File("./webapp" + url.getPath()).toPath());
+            headers = response200Header(body.length);
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
+    private String response200Header(int lengthOfBodyContent) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("HTTP/1.1 200 OK \r\n");
+        sb.append("Content-Type: text/html;charset=utf-8\r\n");
+        sb.append("Content-Length: " + lengthOfBodyContent + "\r\n");
+        sb.append("\r\n");
+
+        return sb.toString();
     }
 
-    private void response302Header(URL url, DataOutputStream dos) {
-        try {
-            dos.writeBytes("HTTP/1.1 302 Found \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Location: " + url.getPath() + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
+    private String response302Header(URL url) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("HTTP/1.1 302 Found \r\n");
+        sb.append("Content-Type: text/html;charset=utf-8\r\n");
+        sb.append("Location: " + url.getPath() + "\r\n");
+        sb.append("\r\n");
+
+        return sb.toString();
     }
 
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
+    public byte[] getBody() {
+        return body;
     }
 
+    public String getHeaders() {
+        return headers;
+    }
 }

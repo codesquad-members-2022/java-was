52220850d9ec080845afd135cb173b2e4565564f
leekm0310 <leekm310@gmail.com>
2022-03-28 11:14:18 +0900

    refactor(DataBase.addUser): Return 문 제거
    * findUserById가 Optional로 반환되도록 변경됨에 따라 수정함

--- src/main/java/db/DataBase.java
+++ src/main/java/db/DataBase.java
@@ -2,6 +2,7 @@ package db;
 
 import java.util.Collection;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 import com.google.common.collect.Maps;
@@ -14,15 +15,12 @@ public class DataBase {
     private static Map<String, User> users = new ConcurrentHashMap<>();
 
     public static void addUser(User user) {
-        if (findUserById(user.getUserId()) == null) {
-            users.put(user.getUserId(), user);
-            return;
-        }
-        throw new DuplicatedUserException(ALREADY_REGISTER_USER_ERROR);
+        findUserById(user.getUserId())
+                .orElseThrow(() -> new DuplicatedUserException(ALREADY_REGISTER_USER_ERROR));
     }
 
-    public static User findUserById(String userId) {
-        return users.get(userId);
+    public static Optional<User> findUserById(String userId) {
+        return Optional.ofNullable(users.get(userId));
     }
 
     public static Collection<User> findAll() {

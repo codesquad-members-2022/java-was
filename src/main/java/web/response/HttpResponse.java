0e59634c066d5863b81f1d969ec02dba9c90c575
ttasjwi <ttasjwi920@gmail.com>
2022-03-30 16:18:35 +0900

    HttpResponse - 쿠키 필드 추가
    - HttpResponse가 쿠키 목록을 List로 가지게 함.
    - addCookie 메서드를 통해 쿠키를 list에 추가
    - responseStaticResource - 로직 과정에서 쿠키를 생성한 경우 cookies에 저장한 쿠키들을 Set-Cookie 헤더의 형태로 응답 기능 추가

--- src/main/java/web/response/HttpResponse.java
+++ src/main/java/web/response/HttpResponse.java
@@ -1,17 +1,27 @@
 package web.response;
 
+import web.common.Cookie;
+
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HttpResponse {
 
     private DataOutputStream dos;
+    private List<Cookie> cookies;
 
     public HttpResponse(OutputStream out) {
         dos = new DataOutputStream(out);
+        cookies = new ArrayList<>();
+    }
+
+    public void addCookie(Cookie cookie) {
+        cookies.add(cookie);
     }
 
     public void redirectTo(String redirectPath) throws IOException {
@@ -24,12 +34,22 @@ public class HttpResponse {
         dos.writeBytes("HTTP/1.1 200 OK \r\n");
         dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
         dos.writeBytes("Content-Length: " + body.length + "\r\n");
+        responseCookies();
         dos.writeBytes("\r\n");
         responseBody(body);
+    }
+
+    private void responseCookies() throws IOException {
+        for (Cookie cookie : cookies) {
+            dos.writeBytes(String.format("Set-Cookie: %s=%s; max-age=%d; path=/\r\n",
+                    cookie.getName(), cookie.getValue(), cookie.getMaxAge())
+            );
+        }
     }
 
     private void responseBody(byte[] body) throws IOException {
         dos.write(body, 0, body.length);
         dos.flush();
     }
+
 }

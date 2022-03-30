883af7d19228398784b2980304c00a993ae42484
geombong <shoy1415@gmail.com>
2022-03-30 11:04:13 +0900

    refactor. run 메서드 분리
    run 메서드 안에서 진행되던 로직 분리

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -5,21 +5,17 @@ import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
+import java.util.HashMap;
 import java.util.Map;
 
 import db.DataBase;
 import model.User;
 import util.HttpRequestUtils;
-import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -35,91 +31,75 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            BufferedReader buf = new BufferedReader(new InputStreamReader(in));
-            String httpHeader = buf.readLine();
-            String token = parseUrl(httpHeader);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
             DataOutputStream dos = new DataOutputStream(out);
-            if (httpHeader.contains("POST")) {
-                int length = 0;
-                while (!httpHeader.equals("")) {
-                    httpHeader = buf.readLine();
-                    if (httpHeader.contains("Content-Length")) {
-                        String contentLength = httpHeader.split(" ")[1];
-                        length = Integer.parseInt(contentLength);
-                    }
-                }
-                String url = urlDecoding(IOUtils.readData(buf, length));
-                User user = createUser(separateUserInfo(url));
-                DataBase.addUser(user);
-                log.debug("user={}", user);
-                response302Header(dos);
+            Request request = createRequest(bufferedReader);
+            Response response = createResponse(request);
+            String httpStateCode = sendResponse(request, response, dos);
+            log.debug("HTTP stateCode={}", httpStateCode);
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private String sendResponse(Request request, Response response, DataOutputStream dos) {
+        if (request.getMethod().equals("POST") && request.getUrl().equals("/user/create")) {
+            User user = createUser(request);
+            log.debug("user={}", user);
+            DataBase.addUser(user);
+            response.header(dos);
+            response.body(dos);
+            return "302";
+        }
+        response.header(dos);
+        response.body(dos);
+        return "200";
+    }
+
+    private Response createResponse(Request request) {
+        return new Response(request.getMethod(), request.getUrl());
+    }
+
+    private Request createRequest(BufferedReader bufferedReader) {
+        String headerLine = "";
+        Map<String, String> headers = new HashMap<>();
+        try {
+            headerLine = bufferedReader.readLine();
+            headers = readHeader(bufferedReader);
+            if (headerLine.contains("POST")) {
+                Request request = new Request(headerLine, headers);
+                request.generateBody(bufferedReader);
+                return request;
             }
-            log.debug("Http httpHeaderLine={}", httpHeader);
-            readHeader(buf, httpHeader);
-            byte[] body = Files.readAllBytes(new File("./webapp" + token).toPath());
-            response200Header(dos, body.length);
-            responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+        return new Request(headerLine, headers);
     }
 
-    private void readHeader(BufferedReader buf, String httpHeader) throws IOException {
-        while (!httpHeader.equals("")) {
-            httpHeader = buf.readLine();
-            log.debug("Http header={}", httpHeader);
+    private Map<String, String> readHeader(BufferedReader bufferedReader) {
+        Map<String, String> headers = new HashMap<>();
+        while (true) {
+            try {
+                String line = bufferedReader.readLine();
+                if (line.equals("")) break;
+                HttpRequestUtils.Pair pair = HttpRequestUtils.parseHeader(line);
+                String key = pair.getKey();
+                String value = pair.getValue();
+                headers.put(key, value);
+            } catch (IOException e) {
+                log.error(e.getMessage());
+            }
         }
+        return headers;
     }
 
-    private String parseUrl(String httpHeader) {
-        return HttpRequestUtils.separateUrl(httpHeader);
-    }
-
-    private User createUser(Map<String, String> urlParseResult) {
-        return new User(urlParseResult.get("userId"),
-                urlParseResult.get("password"),
-                urlParseResult.get("name"),
-                urlParseResult.get("email")
-        );
-    }
-
-    private Map<String, String> separateUserInfo(String token) {
-        Map<String, String> urlParseResult = HttpRequestUtils.parseQueryString(token);
-        log.debug("httpHeaderLine={}", token);
-        return urlParseResult;
-    }
-
-    private String urlDecoding(String url) {
-        return URLDecoder.decode(url, StandardCharsets.UTF_8);
-    }
-
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html; charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
-
-    private void response302Header(DataOutputStream dos) {
-        try {
-            dos.writeBytes("HTTP/1.1 302 Found \r\n");
-            dos.writeBytes("Location: http://localhost:8080/index.html \r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
-
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
+    private User createUser(Request request) {
+        String requestBody = request.getBody();
+        Map<String, String> userData = HttpRequestUtils.parseQueryString(requestBody);
+        return new User(userData.get("userId"),
+                userData.get("password"),
+                userData.get("name"),
+                userData.get("email"));
     }
 }

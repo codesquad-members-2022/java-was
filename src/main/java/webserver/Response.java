b21e3c8a8ace7f1240d47fe8a36f88a623a625e6
geombong <shoy1415@gmail.com>
2022-03-30 11:03:11 +0900

    refactor. Response 클래스 생성
    RequestHandler 클래스에서 Response 부분 분리

---
+++ src/main/java/webserver/Response.java
@@ -0,0 +1,69 @@
+package webserver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class Response {
+    private static final Logger log = LoggerFactory.getLogger(Response.class);
+    private final byte[] body;
+    private final String method;
+    private final String url;
+
+    public Response(String method, String url) {
+        this.method = method;
+        this.url = url;
+        this.body = getFile();
+    }
+
+    private byte[] getFile() {
+        byte[] fileData = new byte[0];
+        try {
+            fileData = Files.readAllBytes(new File("./webapp" + url).toPath());
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+        return fileData;
+    }
+
+    public void header(DataOutputStream dos) {
+        if (this.method.equals("GET")) {
+            response200Header(dos);
+        }
+        response302Header(dos);
+    }
+
+    public void body(DataOutputStream dos) {
+        try {
+            dos.write(body, 0, body.length);
+            dos.flush();
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void response200Header(DataOutputStream dos) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/html; charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + body.length + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void response302Header(DataOutputStream dos) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Found \r\n");
+            dos.writeBytes("Location: http://localhost:8080/index.html \r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+}

4949321ee21890c897f2cca633907d09a6ae7677
ttasjwi <ttasjwi920@gmail.com>
2022-03-30 16:15:09 +0900

    SessionManager 클래스 정의
    - 세션을 관리하는 클래스 SessionManager를 정의했다.
    - 임의의 UUID를 sessionId로 하고, value를 user 객체로 하여 storage에 저장한다.
    - CreateSession 메서드는 사용자를 저장 후 response에 쿠키의 형태로 session 값을 전달한다.

---
+++ src/main/java/web/session/SessionManager.java
@@ -0,0 +1,23 @@
+package web.session;
+
+import model.User;
+import web.common.Cookie;
+import web.response.HttpResponse;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SessionManager {
+
+    private final static Map<String, User> storage = new ConcurrentHashMap<>();
+
+    public static void createSession(User user, HttpResponse response) {
+        String sessionId = UUID.randomUUID().toString();
+        storage.put(sessionId, user);
+        Cookie cookie = new Cookie("sessionId",sessionId);
+        cookie.setMaxAge(1800);
+        response.addCookie(cookie);
+    }
+
+}

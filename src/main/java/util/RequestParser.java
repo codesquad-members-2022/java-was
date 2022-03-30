4fa9206373098faf537e18735699d07dcd27643c
leekm0310 <leekm310@gmail.com>
2022-03-30 10:40:05 +0900

    modify(equestParser.buildRequest): 중복 제거

--- src/main/java/util/RequestParser.java
+++ src/main/java/util/RequestParser.java
@@ -42,21 +42,18 @@ public class RequestParser {
     }
 
     private static HttpRequest buildRequest(BufferedReader reader, Map<String, String> header, RequestLineTokens token) throws IOException {
+        String body = "";
+
         if (token.method.equals("POST")) {
             int contentLength = Integer.parseInt(header.get("Content-Length"));
-            String body = IOUtils.readData(reader, contentLength);
-            return new HttpRequest.Builder().method(token.method)
-                    .path(token.path)
-                    .protocol(token.protocol)
-                    .header(header)
-                    .body(body)
-                    .build();
+            body = IOUtils.readData(reader, contentLength);
         }
 
         return new HttpRequest.Builder().method(token.method)
                 .path(token.path)
                 .protocol(token.protocol)
                 .header(header)
+                .body(body)
                 .build();
     }
 

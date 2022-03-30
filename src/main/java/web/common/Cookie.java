f5f6642183fd34aa4441fa0410385197c6498d4c
ttasjwi <ttasjwi920@gmail.com>
2022-03-30 16:10:46 +0900

    Cookie 클래스 정의
    - name : 쿠키명
    - value : name에 대응되는 값
    - maxAge : 쿠키의 유효기간

---
+++ src/main/java/web/common/Cookie.java
@@ -0,0 +1,29 @@
+package web.common;
+
+public class Cookie {
+
+    private String name;
+    private String value;
+    private int maxAge;
+
+    public Cookie(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public int getMaxAge() {
+        return this.maxAge;
+    }
+
+    public void setMaxAge(int maxAge) {
+        this.maxAge = maxAge;
+    }
+}

2597a99bbb2ce9890c5d8cd0a01af5d9d01beca5
ByeoungJoon Jeon <juni8453@naver.com>
2022-03-30 14:14:41 +0900

    feat) 미션 4 단계 구현을 위한 SessionDataBase 클래스 구현
    1. saveSession()
    User 를 받아와 세션 ID 를 부여합니다.
    
    2. findSessionByUser()
    사용자의 세션을 체크합니다.

---
+++ src/main/java/db/SessionDataBase.java
@@ -0,0 +1,25 @@
+package db;
+
+import com.google.common.collect.Maps;
+import model.User;
+
+import java.util.Map;
+import java.util.UUID;
+
+public class SessionDataBase {
+
+    private static final Map<String, User> sessions = Maps.newHashMap();
+
+    // TODO Response Set-cookie 헤더에 sessionId 를 담아서 보내주는 작업
+    // 세션 저장
+    public String saveSession(User user) {
+        String sessionId = UUID.randomUUID().toString();
+        sessions.put(sessionId, user);
+        return sessionId;
+    }
+
+    // 세션 체크 (로그인 사용자, 게스트 구분 등에 사용 ?)
+    public boolean findSessionByUser(String sessionId) {
+        return sessions.get(sessionId) != null;
+    }
+}

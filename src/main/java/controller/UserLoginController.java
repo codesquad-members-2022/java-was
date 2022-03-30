9e46433a8e9df39962671d961a32666fb83e3ec4
함석호 <kalejuice02@gmail.com>
2022-03-30 12:32:54 +0900

    feat : 로그아웃 요청 시 쿠키의 maxage를 -1로 설정하도록 변경 및 html 파일의 로그아웃 버튼 경로를 "/user/login"으로 변경

--- src/main/java/controller/UserLoginController.java
+++ src/main/java/controller/UserLoginController.java
@@ -10,19 +10,29 @@ import java.io.IOException;
 import java.util.Map;
 
 public class UserLoginController implements Controller{
+
     @Override
     public void service(HttpRequest request, HttpResponse response) throws IOException {
-        Map<String, String> loginInfo = request.getParams();
-        User user = DataBase.findUserById(loginInfo.get("userId"));
-        response.setHttpStatus(HttpStatus.REDIRECT);
-        
-        if (user == null || !user.getPassword().equals(loginInfo.get("password"))) {
-            response.setHeader("Location", "/user/login_failed.html");
-            response.send();
-        } else {
+        if(request.getHttpMethod().equals("POST")){
+            Map<String, String> loginInfo = request.getParams();
+            User user = DataBase.findUserById(loginInfo.get("userId"));
+            response.setHttpStatus(HttpStatus.REDIRECT);
+
+            if (user == null || !user.getPassword().equals(loginInfo.get("password"))) {
+                response.setHeader("Location", "/user/login_failed.html");
+                response.send();
+            } else {
+                response.setHeader("Location", "/index.html");
+                response.setHeader("Set-Cookie", "sessionId=" + user.getUserId() + "; path=/; max-age=60");
+                response.send();
+            }
+        }else{
+            response.setHttpStatus(HttpStatus.REDIRECT);
             response.setHeader("Location", "/index.html");
-            response.setHeader("Set-Cookie", "sessionId=spring; path=/");
+            response.setHeader("Set-Cookie", "sessionId=; path=/; max-age=-1");
             response.send();
         }
+
+
     }
 }

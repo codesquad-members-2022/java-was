c68f563268f251d0f114e59513152b3d2030936d
Minseok-Choi <cms05041@gmail.com>
2022-03-31 15:58:19 +0900

    refactor : index.html logout url매핑 및 Cookie.getSessionId() 수정

--- src/main/java/webserver/Cookie.java
+++ src/main/java/webserver/Cookie.java
@@ -2,6 +2,7 @@ package webserver;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class Cookie {
     private final Map<String, String> cookies = new HashMap<>();
@@ -10,5 +11,7 @@ public class Cookie {
         cookies.put(key, value);
     }
 
-
+    public Optional<String> getSessionId() {
+        return Optional.ofNullable(cookies.get("sessionId"));
+    }
 }

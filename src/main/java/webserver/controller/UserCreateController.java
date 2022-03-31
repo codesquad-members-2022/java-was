81d1908dff02c939039cd9e61e0a6d931fda46d1
Minseok-Choi <cms05041@gmail.com>
2022-03-31 16:02:50 +0900

    refactor : UserCreateController, UserLoginController 로직 수정
    1. UserCreateController signup 실패시 index.html로 리다이렉션 되도록 수정
    2. login 실패 로직 수정 및 쿠키 세팅 로직 구현

--- src/main/java/webserver/controller/UserCreateController.java
+++ src/main/java/webserver/controller/UserCreateController.java
@@ -18,23 +18,27 @@ public class UserCreateController implements Controller {
 
     @Override
     public Response handleRequest(Request request) {
-        String viewPath = signUp(request.getRequestBody());
+        Response response = Response.of(request.getProtocol(), STATUS302);
 
-        return Response.of(request.getProtocol(), STATUS302, viewPath);
+        return signUp(request, response);
     }
 
-    private String signUp(String requestBody) {
-        Map<String, String> params = HttpRequestUtils.parseQueryString(requestBody);
+    private Response signUp(Request request, Response response) {
+        Map<String, String> params = HttpRequestUtils.parseQueryString(request.getRequestBody());
         User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
 
         String userId = user.getUserId();
+
         if (DataBase.findUserById(userId).isPresent()) {
-            throw new IllegalArgumentException("중복된 아이디입니다.");
+            log.debug("{} sign up failed", userId);
+            response.setLocation("/user/form.html");
+            return response;
         }
 
         DataBase.addUser(user);
         log.debug("{} sign up", user);
 
-        return "/index.html";
+        response.setLocation("/index.html");
+        return response;
     }
 }
--- src/main/java/webserver/controller/UserLoginController.java
+++ src/main/java/webserver/controller/UserLoginController.java
@@ -1,6 +1,7 @@
 package webserver.controller;
 
 import db.DataBase;
+import db.SessionDataBase;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +9,7 @@ import util.HttpRequestUtils;
 import webserver.request.Request;
 import webserver.response.Response;
 import java.util.Map;
+import java.util.Optional;
 
 public class UserLoginController implements Controller {
 
@@ -17,27 +19,34 @@ public class UserLoginController implements Controller {
 
     @Override
     public Response handleRequest(Request request) {
-        String viewPath = login(request.getRequestBody());
-
-        return Response.of(request.getProtocol(), STATUS302, viewPath);
+        Response response = Response.of(request.getProtocol(), STATUS302);
+        return login(request, response);
     }
 
-    private String login(String requestBody) {
-        Map<String, String> params = HttpRequestUtils.parseQueryString(requestBody);
+    private Response login(Request request, Response response) {
+        Map<String, String> params = HttpRequestUtils.parseQueryString(request.getRequestBody());
 
         String userId = params.get("userId");
-        User findUser = DataBase.findUserById(userId)
-                .orElseThrow(IllegalArgumentException::new);
-
         String password = params.get("password");
+        Optional<User> userOptional = DataBase.findUserById(userId);
 
-        if (!findUser.isSamePassword(password)) {
-            log.debug("login failed {}", userId);
-            return "/user/login_failed.html";
+
+        if(userOptional.isEmpty()) {
+            response.setLocation("/user/login_failed.html");
+            return response;
         }
 
-        log.debug("login Success {}", userId);
+        User findUser = userOptional.get();
+        if (!findUser.isSamePassword(password)) {
+            response.setLocation("/user/login_failed.html");
+            return response;
+        }
 
-        return "/index.html";
+        String sessionId = SessionDataBase.saveSession(findUser);
+        response.setCookie("sessionId", sessionId);
+        log.debug("login Success {}", userId);
+        response.setLocation("/index.html");
+        return response;
+
     }
 }

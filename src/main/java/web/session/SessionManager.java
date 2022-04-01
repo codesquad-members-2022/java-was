7f7a7ddeba4492e9a3c1eb5551d9293dc8d5edec
ttasjwi <ttasjwi920@gmail.com>
2022-04-01 16:17:54 +0900

    SessionManager - 세션 만료 메서드 추가
    - expireSession : 서버에서 관리하는 세션을 만료시키고, response에 만료쿠키를 addCookie함.

--- src/main/java/web/session/SessionManager.java
+++ src/main/java/web/session/SessionManager.java
@@ -2,6 +2,7 @@ package web.session;
 
 import model.User;
 import web.common.Cookie;
+import web.request.HttpRequest;
 import web.response.HttpResponse;
 
 import java.util.Map;
@@ -12,11 +13,22 @@ public class SessionManager {
 
     private final static Map<String, User> storage = new ConcurrentHashMap<>();
 
+    private static final String SESSION_COOKIE_NAME = "sessionId";
+
     public static void createSession(User user, HttpResponse response) {
         String sessionId = UUID.randomUUID().toString();
         storage.put(sessionId, user);
-        Cookie cookie = new Cookie("sessionId",sessionId);
+        Cookie cookie = new Cookie(SESSION_COOKIE_NAME,sessionId);
         response.addCookie(cookie);
     }
 
+    public static void expireSession(HttpRequest request, HttpResponse response) {
+        Cookie sessionCookie = request.getCookie(SESSION_COOKIE_NAME);
+        String sessionId = sessionCookie.getValue();
+        storage.remove(sessionId);
+
+        Cookie expireCookie = new Cookie(SESSION_COOKIE_NAME, null);
+        expireCookie.setMaxAge(0);
+        response.addCookie(expireCookie);
+    }
 }

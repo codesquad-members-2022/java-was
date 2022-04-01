7109c6a804b25675dc713f96e624c1b033daf076
ttasjwi <ttasjwi920@gmail.com>
2022-04-01 14:38:25 +0900

    Cookie 생성자에 Pair를 사용한 생성자 추가
    - Pair로 Cookie를 생성할 수 있도록 생성자 오버로딩

--- src/main/java/web/common/Cookie.java
+++ src/main/java/web/common/Cookie.java
@@ -1,10 +1,17 @@
 package web.common;
 
+import util.Pair;
+
 public class Cookie {
 
     private String name;
     private String value;
     private Integer maxAge;
+
+    public Cookie(Pair<String, String> pair) {
+        this.name = pair.getKey();
+        this.value = pair.getValue();
+    }
 
     public Cookie(String name, String value) {
         this.name = name;

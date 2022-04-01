836ff040c4d18c6ccd900d4f735af368046e622d
hwicode <khc3796@naver.com>
2022-04-01 11:35:44 +0900

    SignUpController 클래스의 run()메소드가 하나의 기능만을 하도록 리팩토링함

--- src/main/java/controller/SignUpController.java
+++ src/main/java/controller/SignUpController.java
@@ -6,12 +6,10 @@ import http.Response;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import webserver.RequestHandler;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Consumer;
 
 public class SignUpController implements Controller {
 
@@ -34,21 +32,36 @@ public class SignUpController implements Controller {
         responseHeader.put("Content-Length", String.valueOf(body.length));
 
         DataBase.findUserById(request.findBodyByFieldName("userId"))
-                .ifPresentOrElse(user -> {
-                    responseHeader.put("Location", "/user/form.html");
-                    log.debug("중복된 아이디가 존재합니다.");
-                }, () -> {
-                    User user = new User(
-                            request.findBodyByFieldName("userId"),
-                            request.findBodyByFieldName("password"),
-                            request.findBodyByFieldName("name"),
-                            request.findBodyByFieldName("email"));
-                    responseHeader.put("Location", "/index.html");
-                    log.debug("User : {}", user);
-                    DataBase.addUser(user);
-                });
+                .ifPresentOrElse(
+                        redirectSignUpFail(responseHeader),
+                        redirectSignUpSuccess(request, responseHeader)
+                );
         Response response = new Response("HTTP/1.1", "302", "Found", responseHeader, body);
         log.debug("[response] : {}", response.responseMessage());
         return response;
+    }
+
+    private Runnable redirectSignUpSuccess(Request request, Map<String, String> responseHeader) {
+        return () -> {
+            createUser(request);
+            responseHeader.put("Location", "/index.html");
+        };
+    }
+
+    private void createUser(Request request) {
+        User user = new User(
+                request.findBodyByFieldName("userId"),
+                request.findBodyByFieldName("password"),
+                request.findBodyByFieldName("name"),
+                request.findBodyByFieldName("email"));
+        log.debug("User : {}", user);
+        DataBase.addUser(user);
+    }
+
+    private Consumer<User> redirectSignUpFail(Map<String, String> responseHeader) {
+        return user -> {
+            responseHeader.put("Location", "/user/form.html");
+            log.debug("중복된 아이디가 존재합니다.");
+        };
     }
 }
